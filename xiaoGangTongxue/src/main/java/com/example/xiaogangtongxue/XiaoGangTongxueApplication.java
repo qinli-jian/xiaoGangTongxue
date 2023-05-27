59efcddf81ecde6b215816acce30db4db2194d97
qin <2045251631@qq.com>
2023-05-27 14:30:00 +0800

    初始状态

---
+++ xiaoGangTongxue/src/main/java/com/example/xiaogangtongxue/XiaoGangTongxueApplication.java
@@ -0,0 +1,13 @@
+package com.example.xiaogangtongxue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoGangTongxueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoGangTongxueApplication.class, args);
+    }
+
+}
---
+++ xiaoGangTongxue/src/test/java/com/example/xiaogangtongxue/XiaoGangTongxueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xiaogangtongxue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoGangTongxueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
